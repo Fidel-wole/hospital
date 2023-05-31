85f7629749b6893eca6971032cdaba11032e8fd2
Fidel-wole <adewoyeadedayo10@gmail.com>
2023-05-31 13:44:54 +0100

    first commit

---
+++ public/js/data.js
@@ -0,0 +1,35 @@
+const services = [
+    {
+        id: 1,
+        service:'Surgery',
+        about:'Dolor sed tempore similique ipsam nesciunt mollitia porro,excepturi voluptatem, rerum',
+        method:"with technical",
+        logo: 'surgical',
+        link:"services"
+    },
+
+    {
+        id: 3,
+        service:'Neurology',
+        about:'Dolor sed tempore similique ipsam nesciunt mollitia porro,excepturi voluptatem, rerum',
+        method:"with technical",
+        logo:'neurology',
+        link:"services"
+    },
+    {
+        id: 4,
+        service:'Pathology',
+        about:'Dolor sed tempore similique ipsam nesciunt mollitia porro,excepturi voluptatem, rerum',
+        method:"with technical",
+        logo:'femur_alt',
+        link:"services"
+    },
+    {
+        id: 5,
+        service:'Orthopedics',
+        about:'Dolor sed tempore similique ipsam nesciunt mollitia porro excepturi voluptatem, rerum',
+        method:"with technical",
+        logo:'orthopedics',
+        link:"services"
+    },
+]
---
+++ resources/views/publicPage/services.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .none{
+            display: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="surgery none">
+        <h2>surg</h2>
+    </div>
+
+    <div class="neur none">
+        <h2>new</h2>
+    </div>
+</body>
+</html>
